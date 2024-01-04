369d2f23be65d6f0c27141950f592d24b78d827d
KaranGoel59 <karangoel59@gmail.com>
2024-01-04 12:54:05 +0530

    fixed hero jitter

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -16,36 +16,38 @@ export default function Hero() {
 
   return (
     <div className="hero-box">
-      <FontAwesomeIcon icon={hero.icon} className="hero-star" size="4x" />
+      <span className="hidden md:table-cell"><FontAwesomeIcon icon={hero.icon} className="hero-star" size="4x" /></span>
       <div className="hero-name">
-        <span>{hero.about.name}</span>
-        <br/>
-        A &nbsp;
-        <span style={{
-          fontFamily: fontStyle
-        }}>
-        {/** Animation speed 2 sec docs - https://react-type-animation.netlify.app/examples */}
-        <TypeAnimation 
-        preRenderFirstString={true}
-        sequence={
-          hero.about.jobs.map((job, index) => {
-            if (job.styles != undefined) {
-              return [ () => setFontStyle(job.styles.font), job.title, 2000, '']
-            } else {
-              return [() => setFontStyle(hero.defaultStyles.font), job.title, 2000, '']
-            }
-          }).flat()
-        }
-          wrapper="span" 
-          cursor={true} 
-          repeat={Infinity} 
-        />
-        </span>
+        <div className="hero-title">
+          <span>{hero.about.name}</span>
+          <br />
+          A &nbsp;
+          <span style={{
+            fontFamily: fontStyle
+          }}>
+            {/** Animation speed 2 sec docs - https://react-type-animation.netlify.app/examples */}
+            <TypeAnimation
+              preRenderFirstString={true}
+              sequence={
+                hero.about.jobs.map((job, index) => {
+                  if (job.styles != undefined) {
+                    return [() => setFontStyle(job.styles.font), job.title, 2000, '']
+                  } else {
+                    return [() => setFontStyle(hero.defaultStyles.font), job.title, 2000, '']
+                  }
+                }).flat()
+              }
+              wrapper="span"
+              cursor={true}
+              repeat={Infinity}
+            />
+          </span>
+        </div>
         <div className="hero-sub">
           {hero.about.sub}
         </div>
       </div>
-      <FontAwesomeIcon icon={hero.icon} className="hero-star" size="4x" />
+      <span className="hidden md:table-cell"><FontAwesomeIcon icon={hero.icon} className="hero-star" size="4x" /></span>
     </div>
   )
 }
